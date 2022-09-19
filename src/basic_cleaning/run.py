8e7e5b8fea88acad30c1d6a92751e8b3b8be26e3
Yusuke Nakano <nakany@YusukenoMacBook-Air.local>
2022-09-19 11:36:00 +0900

    fix a bug related to location filtering

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logger.debug(f'max:{max_price}')
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
+    # use data with only proper location
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info('export clean_sample.csv to local')
